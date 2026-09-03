dbcec56433dccceade5b137d79a13fae4768b333
Kaveri369 <kaverikapgate@gmail.com>
2026-09-03 23:54:43 +0530

    Create subarraySumWithK.cpp

---
+++ Arrays/subarraySumWithK.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include<iostream>
+using namespace std;
+
+int findAllSubarrayWithGivenSum(vector <int>&arr, int k){
+    map<int, int>mpp;
+    mpp[0] =1;
+    int preSum =0, cnt =0;
+    for(int i=0; i< arr.size(); i++){
+        preSum += arr[i];
+        int remove = preSum-k;
+        cnt += mpp [remove];
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
