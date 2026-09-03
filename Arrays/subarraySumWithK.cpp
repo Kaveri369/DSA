1b0460b781e3e9fac666bfd0f5cf5452ed543115
Kaveri369 <kaverikapgate@gmail.com>
2026-09-03 23:56:10 +0530

    Update subarraySumWithK.cpp

--- Arrays/subarraySumWithK.cpp
+++ Arrays/subarraySumWithK.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int findAllSubarrayWithGivenSum(vector <int>&arr, int k){
-    map<int, int>mpp;
+    unordered_map<int, int>mpp;
     mpp[0] =1;
     int preSum =0, cnt =0;
     for(int i=0; i< arr.size(); i++){
