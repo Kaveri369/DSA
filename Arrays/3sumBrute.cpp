38835bbccfe6ed89a50c8244ee3b170a13719c22
Kaveri369 <kaverikapgate@gmail.com>
2026-09-06 13:50:21 +0530

    Update 3sumBrute.cpp

--- Arrays/3sumBrute.cpp
+++ Arrays/3sumBrute.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
+// vector<vector<int>> triplets(int n, vector<int> &num){
+//     set<vector<int>> st;
+//     for(int i =0; i< n; i++){
+//         for(int j = i+1; j<n; j++){
+//             for(int k =j+1; k< n; k++){
+//                 if(num[i] + num[j]+ num[k]==0){
+//                     // to make sure set store all the unique elements
+//                     vector<int> temp ={ num[i], num[j],num[k]};
+//                     sort(temp.begin(), temp.end());
+//                     st.insert(temp);
+            
+//                 }
+//             }
+//         }
+//     }
+//     vector<vector<int>> ans(st.begin(), st.end());
+//     return ans;
+// }
+
+// better solution:
+
 vector<vector<int>> triplets(int n, vector<int> &num){
     set<vector<int>> st;
-    for(int i =0; i< n; i++){
-        for(int j = i+1; j<n; j++){
-            for(int k =j+1; k< n; k++){
-                if(num[i] + num[j]+ num[k]==0){
-                    // to make sure set store all the unique elements
-                    vector<int> temp ={ num[i], num[j],num[k]};
-                    sort(temp.begin(), temp.end());
-                    st.insert(temp);
-            
-                }
+    for(int i =0; i<n; i++){
+        set<int> hashset;
+        for(int j= i+1; j<n; j++){
+            int third= -(num[i]+num[j]);
+            if(hashset.find(third)!= hashset.end()){
+                vector<int> temp ={num[i], num[j], third};
+                sort(temp.begin(), temp.end());
+                st.insert(temp);
             }
-        }
+            hashset.insert(num[j]);
+        }    
     }
     vector<vector<int>> ans(st.begin(), st.end());
     return ans;
