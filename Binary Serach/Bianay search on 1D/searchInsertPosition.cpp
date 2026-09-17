48bb53806d49007830187803e06b07836cccbfbd
Kaveri369 <kaverikapgate@gmail.com>
2026-09-17 19:42:54 +0530

    Create searchInsertPosition.cpp

---
+++ Binary Serach/Bianay search on 1D/searchInsertPosition.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+         int low = 0, high = n-1;
+    int ans = n;
+    while(low <= high){
+        int mid = (low + high)/ 2;
+        // maybe an answer
+        if(nums[mid] >= target){
+            ans = mid;
+            // look for more small index on left
+            high = mid-1;
+        }
+        else {
+            // look on the right
+            low = mid + 1; 
+        }
+    }
+    return ans;
+    }
+};
